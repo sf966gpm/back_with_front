2b3da832ce73375b34a720f52909d1c41aeba318
sf966gpm <mihailov.box@mail.ru>
2024-01-03 08:22:15 +0000

    feat: add JWT token and AuthController with routes.

--- services/app/src/routes/api.php
+++ services/app/src/routes/api.php
@@ -14,6 +14,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'auth'
+], function ($router) {
+    Route::post('/login', [\App\Http\Controllers\AuthController::class, 'login']);
+    Route::post('/register', [\App\Http\Controllers\AuthController::class, 'register']);
+    Route::post('/logout', [\App\Http\Controllers\AuthController::class, 'logout']);
+    Route::post('/refresh', [\App\Http\Controllers\AuthController::class, 'refresh']);
+    Route::get('/user-profile', [\App\Http\Controllers\AuthController::class, 'userProfile']);
 });
